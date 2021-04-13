83dc920deb3d334ba7c5ac6991394791e986c08f
Kyle Schumacher <kschumacher001@gmail.com>
2021-04-13 10:31:12 -0400

    Add cert for http

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -10,25 +10,25 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class UserController : ControllerBase
+    public class UsersController : ControllerBase
     {
         private readonly DataContext _context;
-        public UserController(DataContext context)
+        public UsersController(DataContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        public  async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            return _context.Users.ToList();
         }
 
         // api/users/id
         [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
+        public ActionResult<AppUser> GetUser(int id)
         {
-            return await _context.Users.FindAsync(id);
+            return _context.Users.Find(id);
         }
     }
 }
