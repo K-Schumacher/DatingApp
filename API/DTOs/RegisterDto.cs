159a49d5b33b893220a440c577fd54d669ea5d14
Kyle Schumacher <kschumacher001@gmail.com>
2021-04-22 12:33:18 -0400

    Added Angular error handling.

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,7 +7,7 @@ namespace API.DTOs
         [Required]
         public string Username { get; set; }
         [Required]
-        [StringLength(20, MinimumLength = 8)]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Your password must be 8 to 20 characters long.")]
         public string Password { get; set; }
     }
 }
