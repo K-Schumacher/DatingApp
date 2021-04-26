998f9c56df88aa7d56b3be2091e1f7bcc33d3c6e
Kyle Schumacher <kschumacher001@gmail.com>
2021-04-25 20:16:45 -0400

    Added member cards and member details

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { User } from './Models/user';
+import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
 
 @Component({
@@ -15,20 +15,11 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient, private accountService: AccountService) {}
   
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser(); 
   }
 
   setCurrentUser() {
-    const user: User = JSON.parse(localStorage.getItem('user') || '{}');
+    const user: User = JSON.parse(localStorage.getItem('user') /*|| '{}'*/);
     this.accountService.setCurrentUser(user);
   }
-
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error)
-    })
-  };
 }
